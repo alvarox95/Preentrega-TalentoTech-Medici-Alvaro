7428b2fb51f7bb2bfeea11dc0b1f35008c9f2517
Alvaro Medici <alvaromedici75@gmail.com>
2024-12-22 20:51:51 -0300

    validacion de la pagina de contacto

---
+++ js/validacion.js
@@ -0,0 +1,24 @@
+const formulario = document.getElementById("formulario-contacto");
+console.log(formulario);
+
+formulario.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const nombre = document.getElementById("nombre").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const mensaje = document.getElementById("mensaje").value.trim();
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  console.log(nombre);
+  console.log(email);
+  console.log(mensaje);
+
+  let formularioValido = true;
+
+  if (nombre === "" || !emailRegex.test(email) || mensaje.length < 10) {
+    formularioValido = false;
+    alert("Todos los campos son obligatorios");
+  } else {
+    alert("Formulario enviado correctamente.");
+  }
+});
